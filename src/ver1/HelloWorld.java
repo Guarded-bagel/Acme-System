62ba0e9826feef5989db9def025ee8feabd441d7
Jacob Bachrach <jbachrach45@gmail.com>
2026-08-29 08:53:52 -0400

    Commiting local change

--- src/ver1/HelloWorld.java
+++ src/ver1/HelloWorld.java
@@ -3,5 +3,6 @@ package ver1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from intelliJ!");
+        System.out.println("Jacob is working locally on his branch.");
     }
 }
