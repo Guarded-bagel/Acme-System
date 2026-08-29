6daed1fa26b27b603fdccb5d502f345f2962b6b3
Jacob Bachrach <jbachrach45@gmail.com>
2026-08-29 11:18:24 -0400

    Added in heyNow method to HelloWorld

--- src/ver1/HelloWorld.java
+++ src/ver1/HelloWorld.java
@@ -1,6 +1,9 @@
 package ver1;
 
 public class HelloWorld {
+    public static void heyNow() {
+        System.out.println("Hey now!");
+    }
     public static void main(String[] args) {
         System.out.println("Hello from intelliJ!");
         System.out.println("Jacob is working locally on his branch.");
